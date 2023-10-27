2b786519e53dc8194ed9e3a150fe407d0971809c
Ataul <ataulmustafa143@gmail.com>
2023-10-27 02:09:40 -0700

    Added code deployement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import SongsComp from '../components/Home/Main';
 import { useNavigate } from 'react-router-dom';
-import toast from 'react-hot-toast';
 
 
 function Home() {
