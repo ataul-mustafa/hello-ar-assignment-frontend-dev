52c77a1af7e1cc524c7a1ec842ba7d2e45fe398c
Ataul <ataulmustafa143@gmail.com>
2023-10-27 02:07:02 -0700

    Added code deployement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import SongsComp from '../components/Home/Main'
+import React, { useEffect } from 'react'
+import SongsComp from '../components/Home/Main';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
 
 function Home() {
+    const navigate = useNavigate()
+    useEffect(()=>{
+        const auth = sessionStorage.getItem('authenticated');
+        if(!auth){
+            navigate("/login")
+        }
+    },[])
   return (
     <div>
         <SongsComp />
